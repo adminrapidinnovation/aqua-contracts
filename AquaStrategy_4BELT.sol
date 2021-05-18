9a95e9403ed7ba34340cf7a82fd06a267f97babc
njrapidinnovation <68365436+njrapidinnovation@users.noreply.github.com>
2021-05-18 15:36:07 +0530

    Update AquaStrategy_4BELT.sol

--- AquaStrategy_4BELT.sol
+++ AquaStrategy_4BELT.sol
@@ -1,3 +1,7 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-05-02
+*/
+
 pragma solidity 0.6.12;
 
 // SPDX-License-Identifier: MIT
@@ -1742,6 +1746,9 @@ abstract contract StratX2 is Ownable, ReentrancyGuard, Pausable {
     uint256 public constant buyBackRateUL = 800;
     address public buyBackAddress = 0x000000000000000000000000000000000000dEaD;
     address public rewardsAddress;
+    
+    address public depositFeeAddress = 0x10BaFF2188e6CEe33e858c6913ef8616Dc0A3e9c;
+    address public withdrawFeeAddress = 0x10BaFF2188e6CEe33e858c6913ef8616Dc0A3e9c;
 
     uint256 public entranceFeeFactor = 9990; // < 0.1% entrance fee - goes to pool + prevents front-running
     uint256 public constant entranceFeeFactorMax = 10000;
@@ -1810,8 +1817,8 @@ abstract contract StratX2 is Ownable, ReentrancyGuard, Pausable {
                 
         uint256 depositFee = _wantAmt.mul(entranceFeeFactorMax.sub(entranceFeeFactor)).div(entranceFeeFactorMax);
         if(depositFee > 0){
-            IERC20(wantAddress).safeIncreaseAllowance(rewardsAddress, depositFee);
-            IERC20(wantAddress).transfer(rewardsAddress, depositFee);
+            IERC20(wantAddress).safeIncreaseAllowance(depositFeeAddress, depositFee);
+            IERC20(wantAddress).transfer(depositFeeAddress, depositFee);
         }
         
         if (isAquaComp) {
@@ -1868,8 +1875,8 @@ abstract contract StratX2 is Ownable, ReentrancyGuard, Pausable {
                 withdrawFeeFactorMax
             );
             uint256 withdrawFee = _wantAmt.mul(withdrawFeeFactorMax.sub(withdrawFeeFactor)).div(withdrawFeeFactorMax);
-            IERC20(wantAddress).safeIncreaseAllowance(rewardsAddress, withdrawFee);
-            IERC20(wantAddress).transfer(rewardsAddress, withdrawFee);
+            IERC20(wantAddress).safeIncreaseAllowance(withdrawFeeAddress, withdrawFee);
+            IERC20(wantAddress).transfer(withdrawFeeAddress, withdrawFee);
         }
 
         if (isAquaComp) {
@@ -2210,6 +2217,8 @@ abstract contract StratX2 is Ownable, ReentrancyGuard, Pausable {
 }
 
 
+
+
 interface IBelt4Pool {
     function add_liquidity(uint256[4] memory amounts, uint256 min_mint_amount)
         external;
@@ -2217,7 +2226,7 @@ interface IBelt4Pool {
 
 contract AquaStrategy_BELT is StratX2 {
     address public belt4PoolAddress;
-
+    address payable public feeAddressesSetter;
     constructor(
         address[] memory _addresses,
         uint256 _pid,
@@ -2266,7 +2275,7 @@ contract AquaStrategy_BELT is StratX2 {
         withdrawFeeFactor = _withdrawFeeFactor;
 
         belt4PoolAddress = _belt4PoolAddress;
-
+        feeAddressesSetter = _msgSender(); 
         transferOwnership(aquaFarmAddress);
     }
 
@@ -2326,5 +2335,16 @@ contract AquaStrategy_BELT is StratX2 {
         lastEarnBlock = block.number;
 
         _farm();
+    }
+
+    function changeFeeAddressSetter(address payable _newFeeAddressSetter) public {
+        require(_msgSender() == feeAddressesSetter,"Access Denied");
+        feeAddressesSetter = _newFeeAddressSetter;
+    }
+    
+    function changeFeeAddress(address _depositFeeAddress,address _withdrawFeeAddress) public {
+        require(_msgSender() == feeAddressesSetter,"Access Denied");
+        depositFeeAddress = _depositFeeAddress;
+        withdrawFeeAddress = _withdrawFeeAddress;
     }
 }
